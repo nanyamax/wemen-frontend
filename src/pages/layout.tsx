efa601aac48b9c01f09465e2d6753c8238b7acf3
nanyamax <ifunanyasunday7@gmail.com>
2023-12-22 04:35:18 +0200

    Refactor

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Menu from '../components/menu';
 import Footer from '../components/footer';
 import { ToastContainer } from 'react-toastify';
--- src/pages/user/component/PeriodCalculator.tsx
+++ src/pages/user/component/PeriodCalculator.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const PeriodCalculator = () => {
   return <div></div>;
 };
