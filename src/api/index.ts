40f52933fc62990255b97222da90b9dc98074cdf
nanyamax <ifunanyasunday7@gmail.com>
2024-01-11 10:18:37 +0200

    fixed api env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// const baseURL = process.env.REACT_APP_BASE_SERVER ??'https://weak-tan-bighorn-sheep.cyclic.app';
-
-const baseURL = 'http://127.0.0.1:5000';
+const baseURL =
+  import.meta.env.VITE_REACT_APP_BASE_SERVER ?? 'https://127.0.0.1:5000';
 
 const apiInstance = axios.create({
   baseURL,
