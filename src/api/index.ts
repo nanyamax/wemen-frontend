f3b7717a78b0866cc3f0f6b14af28b5678f96f83
nanyamax <ifunanyasunday7@gmail.com>
2024-01-12 23:43:05 +0200

    Fixed CORS issue

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const baseURL =
-  import.meta.env.VITE_REACT_APP_BASE_SERVER ?? 'https://127.0.0.1:5000';
+  import.meta.env.VITE_REACT_APP_BASE_SERVER ?? 'http://127.0.0.1:5000';
 
 const apiInstance = axios.create({
   baseURL,
-  withCredentials: true,
 });
 
 export default apiInstance;
